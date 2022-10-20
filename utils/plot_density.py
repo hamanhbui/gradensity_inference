95689f48f724ce39b5029bb21df3b854d0506469
manhhabui <hb.buimanhha@gmail.com>
2022-10-20 00:37:09 -0400

    remove dropout, minimize mean loss, add cVAE

--- utils/plot_density.py
+++ utils/plot_density.py
@@ -7,36 +7,39 @@ import seaborn as sns
 from scipy.stats import wasserstein_distance
 
 
-# with open("../algorithms/VAE/results/plots/Rotated_75_MNIST_0/tr_entropies.pkl", "rb") as fp:
-#     train_NLL = pickle.load(fp)
-
-# with open("../algorithms/VAE/results/plots/Rotated_75_MNIST_0/te_entropies.pkl", "rb") as fp:
-#     test_NLL = pickle.load(fp)
-
-# with open("../algorithms/VAE/results/plots/Rotated_75_MNIST_0/adapt_entropies.pkl", "rb") as fp:
-#     adapted_NLL = pickle.load(fp)
-
-with open("../algorithms/VAE/results/plots/Rotated_75_MNIST_0/tr_nlls.pkl", "rb") as fp:
+with open("../algorithms/VAE/results/plots/Rotated_75_MNIST_4/tr_entropies.pkl", "rb") as fp:
     train_NLL = pickle.load(fp)
 
-with open("../algorithms/VAE/results/plots/Rotated_75_MNIST_0/te_nlls.pkl", "rb") as fp:
+with open("../algorithms/VAE/results/plots/Rotated_75_MNIST_4/te_entropies.pkl", "rb") as fp:
     test_NLL = pickle.load(fp)
 
-with open("../algorithms/VAE/results/plots/Rotated_75_MNIST_0/adapt_nlls.pkl", "rb") as fp:
+with open("../algorithms/VAE/results/plots/Rotated_75_MNIST_4/adapt_entropies.pkl", "rb") as fp:
     adapted_NLL = pickle.load(fp)
 
+# # seaborn histogram
 plt.figure(figsize=(20, 10))
 plt.xlabel("Entropy")
-plt.hist(train_NLL, label="train", density=True, bins=int(180 / 5))
-plt.hist(test_NLL, label="test", density=True, bins=int(180 / 5))
-plt.hist(adapted_NLL, label="adapt", density=True, bins=int(180 / 5))
+sns.kdeplot(train_NLL, label="train")
+sns.kdeplot(test_NLL, label="test")
+sns.kdeplot(adapted_NLL, label="adapted")
+
 plt.legend()
-plt.savefig("out/out.png")
+plt.savefig("out/predictive_entropy.png")
 
-# # seaborn histogram
-# sns.kdeplot(train_NLL, label="train")
-# sns.kdeplot(test_NLL, label="test")
-# sns.kdeplot(adapted_NLL, label="adapted")
+# with open("../algorithms/VAE/results/plots/Rotated_75_MNIST_4/tr_nlls.pkl", "rb") as fp:
+#     train_NLL = pickle.load(fp)
 
+# with open("../algorithms/VAE/results/plots/Rotated_75_MNIST_4/te_nlls.pkl", "rb") as fp:
+#     test_NLL = pickle.load(fp)
+
+# with open("../algorithms/VAE/results/plots/Rotated_75_MNIST_4/adapt_nlls.pkl", "rb") as fp:
+#     adapted_NLL = pickle.load(fp)
+
+# plt.figure(figsize=(20, 10))
+# plt.xlabel("Log density")
+# plt.hist(train_NLL, label="train", density=True, bins=int(180 / 5))
+# plt.hist(test_NLL, label="test", density=True, bins=int(180 / 5))
+# plt.hist(adapted_NLL, label="adapt", density=True, bins=int(180 / 5))
 # plt.legend()
-# plt.savefig("out/predictive_entropy.png")
+# plt.savefig("out/out.png")
+
